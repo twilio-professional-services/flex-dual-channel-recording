9a930b6ecf427dd174b9d3c84bf15b2dcea24d0e
Jim Madden <jmadden@twilio.com>
2022-01-06 12:34:26 -0800

    udpated code to record customer or worker depending on .env settings

--- plugin-dual-channel-recording/src/listeners/CustomListeners.js
+++ plugin-dual-channel-recording/src/listeners/CustomListeners.js
@@ -3,6 +3,7 @@ import { ParticipantType, ReservationEvents } from '../enums';
 
 const manager = Manager.getInstance();
 const reservationListeners = new Map();
+const { REACT_APP_RECORD_CHANNEL } = process.env;
 
 const startCallRecording = async (callSid) => {
   console.debug('Creating recording for call SID:', callSid);
@@ -10,14 +11,14 @@ const startCallRecording = async (callSid) => {
 
   const fetchBody = {
     Token: manager.store.getState().flex.session.ssoTokenPayload.token,
-    callSid
+    callSid,
   };
   const fetchOptions = {
     method: 'POST',
     body: new URLSearchParams(fetchBody),
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    }
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
   };
 
   let recording;
@@ -30,7 +31,7 @@ const startCallRecording = async (callSid) => {
   }
 
   return recording;
-}
+};
 
 const addCallDataToTask = async (task, callSid, recording) => {
   const { attributes, conference } = task;
@@ -44,7 +45,7 @@ const addCallDataToTask = async (task, callSid, recording) => {
     // Outbound calls initiated from Flex (via StartOutboundCall Action)
     // do not include call_sid and conference metadata in task attributes
     newAttributes.conference = { sid: conference.conferenceSid };
-    
+
     // callSid will be undefined if the outbound call was ended before
     // the called party answered
     newAttributes.call_sid = callSid;
@@ -74,23 +75,29 @@ const addCallDataToTask = async (task, callSid, recording) => {
     // worker leg, which could result in multiple recordings per call in the case
     // of a transfer, then you'll want to use the reservation_attributes pattern:
     // https://www.twilio.com/docs/flex/developer/insights/custom-media-attached-conversations#add-media-links
-    newAttributes.conversations = {
-      ...conversations,
-      media: [
-        {
-          url: recordingUrl,
-          type: 'VoiceRecording',
-          start_time: recordingStartTime,
-          channels: [ 'customer', 'others' ]
-        }
-      ]
+    const media = {
+      url: recordingUrl,
+      type: 'VoiceRecording',
+      start_time: recordingStartTime,
+      channels: ['customer', 'others'],
     };
+    if (newAttributes.conversations.media.length === 0) {
+      newAttributes.conversations = {
+        ...conversations,
+        media: [mediaObj],
+      };
+    } else {
+      newAttributes.conversations.media.push(mediaObj);
+      newAttributes.conversations = {
+        ...conversations,
+      };
+    }
   }
 
   if (shouldUpdateTaskAttributes) {
     await task.setAttributes(newAttributes);
   }
-}
+};
 
 const isTaskActive = (task) => {
   const { sid: reservationSid, taskStatus } = task;
@@ -99,50 +106,59 @@ const isTaskActive = (task) => {
   } else {
     return manager.workerClient.reservations.has(reservationSid);
   }
-}
+};
 
-const waitForConferenceParticipants = (task) => new Promise(resolve => {
-  const waitTimeMs = 100;
-  // For outbound calls, the customer participant doesn't join the conference 
-  // until the called party answers. Need to allow enough time for that to happen.
-  const maxWaitTimeMs = 60000;
-  let waitForConferenceInterval = setInterval(async () => {
-    const { conference } = task;
+const waitForConferenceParticipants = (task) =>
+  new Promise((resolve) => {
+    const waitTimeMs = 100;
+    // For outbound calls, the customer participant doesn't join the conference
+    // until the called party answers. Need to allow enough time for that to happen.
+    const maxWaitTimeMs = 60000;
+    let waitForConferenceInterval = setInterval(async () => {
+      const { conference } = task;
 
-    if (!isTaskActive(task)) {
-      console.debug('Call canceled, clearing waitForConferenceInterval');
+      if (!isTaskActive(task)) {
+        console.debug('Call canceled, clearing waitForConferenceInterval');
+        waitForConferenceInterval = clearInterval(waitForConferenceInterval);
+        return;
+      }
+      if (conference === undefined) {
+        return;
+      }
+      const { participants } = conference;
+      if (Array.isArray(participants) && participants.length < 2) {
+        return;
+      }
+      const worker = participants.find(
+        (p) => p.participantType === ParticipantType.worker
+      );
+      const customer = participants.find(
+        (p) => p.participantType === ParticipantType.customer
+      );
+
+      if (!worker || !customer) {
+        return;
+      }
+
+      console.debug('Worker and customer participants joined conference');
       waitForConferenceInterval = clearInterval(waitForConferenceInterval);
-      return;
-    }
-    if (conference === undefined) {
-      return;
-    }
-    const { participants } = conference;
-    if (Array.isArray(participants) && participants.length < 2) {
-      return;
-    }
-    const worker = participants.find(p => p.participantType === ParticipantType.worker);
-    const customer = participants.find(p => p.participantType === ParticipantType.customer);
 
-    if (!worker || !customer) {
-      return;
-    }
+      resolve(participants);
+    }, waitTimeMs);
 
-    console.debug('Worker and customer participants joined conference');
-    waitForConferenceInterval = clearInterval(waitForConferenceInterval);
+    setTimeout(() => {
+      if (waitForConferenceInterval) {
+        console.debug(
+          `Customer participant didn't show up within ${
+            maxWaitTimeMs / 1000
+          } seconds`
+        );
+        clearInterval(waitForConferenceInterval);
 
-    resolve(participants);
-  }, waitTimeMs);
-
-  setTimeout(() => {
-    if (waitForConferenceInterval) {
-      console.debug(`Customer participant didn't show up within ${maxWaitTimeMs / 1000} seconds`);
-      clearInterval(waitForConferenceInterval)
-
-      resolve([])
-    }
-  }, maxWaitTimeMs);
-});
+        resolve([]);
+      }
+    }, maxWaitTimeMs);
+  });
 
 const addMissingCallDataIfNeeded = async (task) => {
   const { attributes } = task;
@@ -153,7 +169,7 @@ const addMissingCallDataIfNeeded = async (task) => {
     // have the desired call and conference metadata
     await addCallDataToTask(task);
   }
-}
+};
 
 Actions.addListener('beforeCompleteTask', async (payload) => {
   // Listening for this event as a last resort check to ensure call
@@ -164,13 +180,13 @@ Actions.addListener('beforeCompleteTask', async (payload) => {
 Actions.addListener('beforeHangupCall', async (payload) => {
   // Listening for this event to at least capture the conference SID
   // if the outbound call is canceled before the called party answers
-  addMissingCallDataIfNeeded(payload.task)
+  addMissingCallDataIfNeeded(payload.task);
 });
 
 const handleAcceptedCall = async (task) => {
   const { attributes } = task;
   const { conversations } = attributes;
-  
+
   if (conversations && conversations.media) {
     // This indicates a recording has already been started for this call
     // and all relevant metadata should already be on task attributes
@@ -181,18 +197,21 @@ const handleAcceptedCall = async (task) => {
   // conference before we start the recording
   console.debug('Waiting for customer and worker to join the conference');
   const participants = await waitForConferenceParticipants(task);
+  const participantLeg = participants.find(
+    (p) => p.participantType === ParticipantType.REACT_APP_RECORD_CHANNEL
+  );
 
-  const customer = participants.find(p => p.participantType === ParticipantType.customer);
-
-  if (!customer) {
-    console.warn('No customer participant. Not starting the call recording');
+  if (!participantLeg) {
+    console.warn(
+      'No customer or worker participant. Not starting the call recording'
+    );
     return;
   }
 
   // Choosing to record the customer call SID here. If you want to record
   // the worker leg of the call instead, adjust the logic above to find
   // the worker participant and use that call SID instead.
-  const { callSid } = customer;
+  const { callSid } = participantLeg;
 
   const recording = await startCallRecording(callSid);
   await addCallDataToTask(task, callSid, recording);
@@ -204,14 +223,14 @@ const handleReservationAccepted = async (reservation) => {
   if (TaskHelper.isCallTask(task)) {
     await handleAcceptedCall(task);
   }
-}
+};
 
 const handleReservationUpdated = (event, reservation) => {
   console.debug('Event, reservation updated', event, reservation);
   switch (event) {
     case ReservationEvents.accepted: {
       handleReservationAccepted(reservation);
-      break; 
+      break;
     }
     case ReservationEvents.wrapup:
     case ReservationEvents.completed:
@@ -230,7 +249,7 @@ const handleReservationUpdated = (event, reservation) => {
 const stopReservationListeners = (reservation) => {
   const listeners = reservationListeners.get(reservation);
   if (listeners) {
-    listeners.forEach(listener => {
+    listeners.forEach((listener) => {
       reservation.removeListener(listener.event, listener.callback);
     });
     reservationListeners.delete(reservation);
@@ -238,10 +257,12 @@ const stopReservationListeners = (reservation) => {
 };
 
 const initReservationListeners = (reservation) => {
-  const trueReservation = reservation.addListener ? reservation : reservation.source;
+  const trueReservation = reservation.addListener
+    ? reservation
+    : reservation.source;
   stopReservationListeners(trueReservation);
   const listeners = [];
-  Object.values(ReservationEvents).forEach(event => {
+  Object.values(ReservationEvents).forEach((event) => {
     const callback = () => handleReservationUpdated(event, trueReservation);
     trueReservation.addListener(event, callback);
     listeners.push({ event, callback });
@@ -258,6 +279,6 @@ const handleReservationCreated = (reservation) => {
   handleNewReservation(reservation);
 };
 
-manager.workerClient.on('reservationCreated', reservation => {
+manager.workerClient.on('reservationCreated', (reservation) => {
   handleReservationCreated(reservation);
 });
